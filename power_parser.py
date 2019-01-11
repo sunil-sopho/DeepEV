382d3724035e766e431562658b547efb83d8b41f
Prabhat1808 <prabhatkanaujia08@gmail.com>
2019-01-12 00:49:44 +0530

    parsed power usage files

---
+++ power_parser.py
@@ -0,0 +1,38 @@
+import sys
+import csv
+
+args = sys.argv
+filename = args[1]
+raw_file = '--' + ''.join(map(str,open(filename,'rb').readlines())) + '--0'
+
+sections = []
+tmp = ''
+count = 0
+
+for ch in raw_file:
+    if (ch != '-'):
+        tmp += ch
+        if(count > 1):
+            sections += [tmp]
+            tmp = ''
+        count = 0
+    else:
+         count += 1
+
+data = sections[-1]
+data = data.split('\\n\'b\'')
+data.pop(0)
+data.pop(-1)
+
+data_tabular = []
+for row in data:
+    extracted = row.split('\\t')
+    if(len(extracted)!=0):
+        extracted.pop(1)
+        extracted.pop(-2)
+        data_tabular += [extracted]
+
+directory = args[2]
+with open(directory + "/" + (((filename.split('/'))[-1]).split('.'))[0] + '.csv','w') as my_csv:
+    writer = csv.writer(my_csv,delimiter=',')
+    writer.writerows(data_tabular)
