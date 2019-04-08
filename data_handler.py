8808ddd31aa6aa4ebc6a2431425a0759516023fd
Prabhat1808 <prabhatkanaujia08@gmail.com>
2019-04-08 16:08:18 +0530

    timeline sync func added

--- data_handler.py
+++ data_handler.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 
 PARENT = "./datasets/"
-DIRECTORIES = {"1":"1/trips/","2":"1/power/","3":"2/HOD/","4":"2/HOEP/","5":"3/","6":"4/"}
-SUBFILES = {"2":["power_weekday.csv","power_weekend.csv"],"3":["PUB_Demand_2008.csv","PUB_Demand_2008_v1.csv","PUB_Demand_2009.csv","PUB_Demand_2009_v1.csv"],"4":["PUB_PriceHOEPPredispOR_2008.csv","PUB_PriceHOEPPredispOR_2008_v1.csv","PUB_PriceHOEPPredispOR_2009.csv","PUB_PriceHOEPPredispOR_2009_v1.csv"],"5":["2008_weather_station_data.csv","2009_weather_station_data.csv"],"6":["fueltypesall.csv"]}
+DIRECTORIES = {"1":"1/trips/","2":"1/power/","3":"2/HOD/","4":"2/HOEP/","5":"3/","6":"4/","7":"1/base/"}
+SUBFILES = {"2":["power_weekday.csv","power_weekend.csv"],"3":["PUB_Demand_2008.csv","PUB_Demand_2008_v1.csv","PUB_Demand_2009.csv","PUB_Demand_2009_v1.csv"],"4":["PUB_PriceHOEPPredispOR_2008.csv","PUB_PriceHOEPPredispOR_2008_v1.csv","PUB_PriceHOEPPredispOR_2009.csv","PUB_PriceHOEPPredispOR_2009_v1.csv"],"5":["2008_weather_station_data.csv","2009_weather_station_data.csv"],"6":["fueltypesall.csv"],"7":["Weekday duty cycle_86400 Sec.csv","Weekend duty cycle_86400 Sec.csv"]}
 BASEFILES = {"2":["",""]}
 # 1 : trips
 # 2 : power
@@ -28,8 +28,38 @@ def get_data(arguments):
         dir = DIRECTORIES[arg1]
         data = []
         for file in SUBFILES[arg1]:
-            data = data + [pd.read_csv(DIRECTORIES[arg1]+file,error_bad_lines=False)]
+            data = data + [pd.read_csv(DIRECTORIES[arg1]+file,error_bad_lines=False,header=None)]
     # print(data)
     return data
 
-# def sync_timeline():
+def sync_power_usage(base, to_sync):
+    dat1 = get_data(base)
+    dat2 = get_data(to_sync)
+    synced = []
+    deft = [0, 0]
+
+    for i in range(2):
+        iterator = 0
+        delimiter = 0
+        df = pd.DataFrame(0,index=np.arange(dat1[i].size),columns=['Speed','Power'])
+
+        print ("i = ", i)
+        for j in range(dat1[i].size):
+            print (j)
+            if(dat1[i].iloc[j,0] == 0):
+                df.loc[j] = deft
+                if(delimiter == 1):
+                    delimiter = 0
+            else:
+                if(delimiter == 0):
+                    delimiter = 1
+
+            if(delimiter == 1):
+                while(dat2[i].iloc[iterator,1] == 0):
+                    iterator = iterator + 1
+                df.loc[j] = [dat2[i].iloc[iterator,1],dat2[i].iloc[iterator,2]]
+                iterator = iterator + 1
+
+        synced.append(df)
+
+    return synced
